bc33605ebfa8821bd7535828eb38f2bc97cc45be
DarkLord1111 <45848763+DarkLord1111@users.noreply.github.com>
2018-12-29 12:54:31 +0100

    Update Questions.py

--- Questions.py
+++ Questions.py
@@ -13,3 +13,18 @@ def askYN(question:str,default='')->bool:
                 response=default if default in ('y','n') else '0'
 
     return response[0]=='y'
+
+
+
+def ask_option(question:str, options:list): # es ask_option('choose the box: ', boxes)
+    """
+    Asks the user to chose one of the provided options until exact input is given.\n
+    <question> : The question to ask\n
+    <options>  : Can use elements of a variable (suggestion: delete '[' ']: ') or hardcoded into the question (eg: 'a','b','c')
+    """
+    response='0'
+    while response[0] not in options:
+        response = input(question + '[' + str(options)+ ']: ')
+        if not response or response.isspace():
+            response = '0'
+    return response[0] #True, return chosen option
